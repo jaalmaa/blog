f59fad6a55fa3224e45ed14bf1a55e5a9f9e41d1
jaalma <james@jmaclachlan.me>
2022-12-04 17:14:37 -0500

    remove netlify driver, it broke stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), tailwind()],
-  output: "server",
-  adapter: netlify()
+  output: 'static'
 });
