605444291ccab508d3cd644adf5a6f19afbeea69
jaalma <james@jmaclachlan.me>
2022-12-04 16:31:44 -0500

    add netlify deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
-
 import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify/functions";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind()]
+  integrations: [mdx(), tailwind()],
+  output: "server",
+  adapter: netlify()
 });
