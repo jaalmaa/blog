063c38eab91938c265aafee3c36a8c8d33a1a08d
jaalma <james@jmaclachlan.me>
2023-09-03 15:44:06 -0400

    update to astro 3

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		fontFamily: {
-			sans: ['Graphik', 'sans-serif'],
-			serif: ['Merriweather', 'serif']
-		},
-		extend: {
-			colors: {
-				'purple-main': 'bg-purple-500'
-			}
-		},
-	},
-	plugins: [
-		require('@tailwindcss/line-clamp')
-	],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      colors: {
+        "purple-main": "bg-purple-500",
+      },
+    },
+  },
+  plugins: [],
+};
